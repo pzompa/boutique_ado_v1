d374367d5ee3eac7e4556c90ee56ba93aab173eb
pzompa <>
2023-07-27 11:05:11 +0000

    Added search functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-pzompa-boutique-ado-v1-s9qokumpr2.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8001-pzompa-boutique-ado-v1-43xt2kvlpx.us2.codeanyapp.com']
 
 
 # Application definition
