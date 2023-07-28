e0cf083114f5edc039f84bf9a7488435f57be604
pzompa <>
2023-07-28 09:32:57 +0000

    Added sorting and back to top link

--- products/views.py
+++ products/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
-
 # Create your views here.
-
 
 def all_products(request):
     """ A view to return all products including sorting and search queries """
@@ -23,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -35,7 +35,6 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -45,13 +44,11 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}-{direction}'
-
-
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
-        'current categories': categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
