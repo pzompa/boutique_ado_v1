bb686a8c0a7bcf6095732fb5822577ad58d44bb4
pzompa <>
2023-08-12 12:25:02 +0000

    Added forms, admin and signals.

--- checkout/admin.py
+++ checkout/admin.py
@@ -8,15 +8,23 @@ class OrderLineItemAdminInline(admin.TabularInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ('order_number', 'date', 'delivery_cost',
-                       'order_total', 'grand_total',)
+    inlines = (OrderLineItemAdminInline,)
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number',
-              'postcode', 'town_or_city', 'street_address1', 'street_address2',
-              'county', 'delivery_cost', 'order_total', 'grand_total',)
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
 
-    
-list_display = ('order_number', 'date', 'full_name', 'order_total',
-                'delivery_cost', 'grand_total',)
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
 
-ordering = ('-date',)
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
