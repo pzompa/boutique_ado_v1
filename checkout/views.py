5887a6864b99a436e9b0243293c3f07a732c3993
pzompa <>
2023-08-16 08:16:16 +0000

    updated

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NeKtYAwpLA9i7SBGTf0hyMiDsU3AEAa5RUspjksNbdOKel9Ff3X1DWXrx5nX4OrwbCBXVSk2MAJ1gXD67N0HyrZ00aOvCZNKi',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
